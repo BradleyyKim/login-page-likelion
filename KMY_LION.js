fe6a24276076bdf619bcd51e65cae401a96da49f
김민영 <win930323@gmail.com>
2022-03-16 01:59:20 +0900

    toggle added.

--- KMY_LION.js
+++ KMY_LION.js
@@ -1,10 +1,25 @@
-const img = document.querySelector(".toggle_img").src;
-document.querySelector(".login_status").addEventListener("click", () => {
-  if (img == "/img/icon_check_empty.png") {
-    img.src = "/img/icon_check.png";
-  } else {
-    img.src = "/img/icon_check_empty.png";
-  }
-});
+// const img = document.querySelector(".toggle_img").src;
+// document.querySelector(".login_status").addEventListener("click", () => {
+//   if (img == "/img/icon_check_empty.png") {
+//     img.src = "/img/icon_check.png";
+//   } else {
+//     img.src = "/img/icon_check_empty.png";
+//   }
+// });
 
-console.log(img)
+// console.log(img)
+var toggleImg = "img1";
+
+function toggle() {
+  const image = document.getElementById("toggle_image");
+  const buttonText = document.querySelector(".login_status");
+  if (toggleImg === "img1") {
+    image.src = "/img/icon_check.png";
+    buttonText.style.color = "#2e80ec";
+    toggleImg = "img2";
+  } else {
+    image.src = "/img/icon_check_empty.png";
+    buttonText.style.color = "#6f6f6f";
+    toggleImg = "img1";
+  }
+}
